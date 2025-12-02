5dbe5442590dba84c6bb39ba37fb3f1c9a8ea3ab
John Putnam <johnputnam4.5@gmail.com>
2025-12-02 16:00:20 -0500

    imgdisplay

--- TauCamFinal/Debug/Core/Src/main.d
+++ TauCamFinal/Debug/Core/Src/main.d
@@ -27,7 +27,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_uart.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_exti.h \
  ../Core/Inc/i2c.h ../Core/Inc/main.h ../Core/Inc/spi.h \
- ../Core/Inc/usart.h ../Core/Inc/gpio.h ../Core/Inc/ILI9341_hal.h
+ ../Core/Inc/usart.h ../Core/Inc/gpio.h ../Core/Inc/ILI9341_hal.h \
+ ../Core/Inc/tiger_rgb565.h
 ../Core/Inc/main.h:
 ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal.h:
 ../Core/Inc/stm32l1xx_hal_conf.h:
@@ -62,3 +63,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/usart.h:
 ../Core/Inc/gpio.h:
 ../Core/Inc/ILI9341_hal.h:
+../Core/Inc/tiger_rgb565.h:
