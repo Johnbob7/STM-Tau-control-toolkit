3024d2efad29db702b2b8c6740c2a4d5d1003f52
SillyGesture <69798195+Johnbob7@users.noreply.github.com>
2025-12-03 18:59:51 -0500

    Add Tau LUT console controls

--- stmhardwareproject/Core/Inc/camcontrol.h
+++ stmhardwareproject/Core/Inc/camcontrol.h
@@ -13,6 +13,22 @@ void CamControl_Task(void);
 void Tau_SendSetFfcAuto(void);
 void Tau_SendGetFfcMode(void);
 void Tau_ReadAndPrintReply(void);
+void Tau_GetVideoLUT(void);
+void Tau_SetLUT_WhiteHot(void);
+void Tau_SetLUT_BlackHot(void);
+void Tau_SetLUT_Fusion(void);
+void Tau_SetLUT_Rainbow(void);
+void Tau_SetLUT_Globow(void);
+void Tau_SetLUT_Ironbow1(void);
+void Tau_SetLUT_Ironbow2(void);
+void Tau_SetLUT_Sepia(void);
+void Tau_SetLUT_Color1(void);
+void Tau_SetLUT_Color2(void);
+void Tau_SetLUT_IceFire(void);
+void Tau_SetLUT_Rain(void);
+void Tau_SetLUT_Custom1(void);
+const char *Tau_LUT_NameFromId(uint16_t lut);
+void Tau_PrintHelp(void);
 void dbg_printf(const char *fmt, ...);
 
 #endif
