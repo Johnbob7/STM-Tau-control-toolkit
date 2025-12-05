a61f704259aa33b3486031cd1801f103c207ddf7
John Putnam <johnputnam4.5@gmail.com>
2025-12-05 13:44:15 -0500

    removed lab3

--- Lab 3/Core/Inc/adc_keypad.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADC_KEYPAD_H
-#define ADC_KEYPAD_H
-
-void ADC_ButtonInit(void);
-char GetButton(void);
-
-#endif
--- Lab 3/Core/Inc/lab3Application.h
+++
@@ -1,7 +0,0 @@
-// lab3Application.h
-#ifndef LAB3APPLICATION_H
-#define LAB3APPLICATION_H
-
-void start_app(void);  // Assembly entry point
-
-#endif
